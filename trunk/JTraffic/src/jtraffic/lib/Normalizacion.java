a34ec1fad08e4c520e4ff1b87e5536d87d6c1022
juanmasp <juanmasp@44508566-b4cd-11dd-818f-3bd198e1d18e>
2008-12-17 11:17:08 +0000

    Convertida en versión de pruebas. Añadidos huecos para rellenar el algoritmo pasito a pasito.

---
+++ trunk/JTraffic/src/jtraffic/lib/Normalizacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtraffic.lib;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author JuanmaSP
+ */
+public class Normalizacion {
+    /*
+    public static BufferedImage aplicar(BufferedImage imagen){
+        
+    }*/
+}
---
+++ trunk/JTraffic/src/jtraffic/lib/SumaImagenes.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtraffic.lib;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ *
+ * @author JuanmaSP
+ */
+public class SumaImagenes {
+    /*
+    public static BufferedImage sumaImagenes(BufferedImage a, BufferedImage b){
+
+    }
+
+    public static BufferedImage sumaImagenes(List<BufferedImage> imagenes){
+
+    }
+
+    public static BufferedImage sumaImagenesNormalizadas(BufferedImage a, BufferedImage b){
+        
+    }
+
+    public static BufferedImage sumaImagenesNormalizadas(List<BufferedImage> imagenes){
+
+    }*/
+}
