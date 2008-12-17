a3dc4ce5c473091e139d9a74f8d2fe37a2221963
juanmasp <juanmasp@44508566-b4cd-11dd-818f-3bd198e1d18e>
2008-12-17 12:42:05 +0000

    Puesto bonito en general.

---
+++ trunk/JTraffic/src/jtraffic/gui/events/MouseListenerImagenesLabel.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtraffic.gui.events;
+
+import java.awt.Frame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import jtraffic.ImageDialog;
+
+/**
+ *
+ * @author JuanmaSP
+ */
+public class MouseListenerImagenesLabel implements MouseListener{
+    private Frame creador;
+    private BufferedImage image;
+    private boolean modo;
+
+    public MouseListenerImagenesLabel(Frame creador, BufferedImage imagen, boolean modo){
+        super();
+        this.creador = creador;
+        this.image = imagen;
+        this.modo = modo;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        ImageDialog dialog = new ImageDialog(image, creador, modo);
+        dialog.show();
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+}
